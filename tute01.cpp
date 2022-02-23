599a2b2843f4f968e34420ef7bbe704f7e688a92
Devin Lakvindu <99410714+IT21215056@users.noreply.github.com>
2022-02-23 08:06:34 +0000

    Changed c programs to c++

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,17 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 int main() 
 {
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+  float cm, inches;
+
+  cout << "Enter length in cm : ";
+  cin>>cm;
+
+  inches = cm /2.54;
+
+  cout << "Value is : "<< inches;
     return 0;
 }  
